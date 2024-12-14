aaa853605556d4abbe90a06ba43c18e6da787f12
amitchavan6322 <amitchavan6322@gmail.com>
2024-12-14 20:50:37 +0530

    rearranging

--- loops-patterns/src/LeftHalfPyramid.java
+++ loops-patterns/src/LeftHalfPyramid.java
@@ -10,15 +10,14 @@ public class LeftHalfPyramid {
 
     public static void main(String[] args) {
         for (int i = 0; i <= 5; i++) {
-           // System.out.println("i = " +i);
+
             for (int j = 0; j < 5-i; j++) {
-                //System.out.println("j = " +j);
                 System.out.print(" ");
             }
                 for(int k = 0; k<i; k++){
-                   // System.out.println("k = " +k);
                     System.out.print("*");
                 }
+
             System.out.println("");
         }
     }
