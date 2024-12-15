8d550394e9652bd3634ec07a905960bc62f4534a
amitchavan6322 <amitchavan6322@gmail.com>
2024-12-15 21:14:37 +0530

    pushing changes

--- loops/src/patterns/SquareHallowPattern.java
+++ loops/src/patterns/SquareHallowPattern.java
@@ -10,12 +10,21 @@ public class SquareHallowPattern {
         // *       *
         // * * * * *
 
-        int i;
-
+        int i, j;
+ int n = 5, m = 5;
         // loop to iterate for rows
-        for (i = 0; i <= 5; i++) {
-            System.out.print("*");
+        for (i = 1; i <= n; i++) {
+
+            for (j = 1; j <= m; j++) {
+                if (i == 1 || i == n ||
+                        j == 1 || j == m)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
         }
 
     }
 }
+
